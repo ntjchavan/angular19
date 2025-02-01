9b3a76d786bbfb2aff63c847e71646df79fcaa23
ntjchavan <ntjchavan@gmail.com>
2025-02-01 22:44:08 +0530

    Added navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: HomeComponent },
+    { path: '', component: AboutusComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'aboutus', component: AboutusComponent },
+];
