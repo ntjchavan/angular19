5bc92e289ddd4994115dc59e396c5d82122cb62f
ntjchavan <ntjchavan@gmail.com>
2025-02-02 15:32:16 +0530

    Added notification icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
-    // { path: '', component: HomeComponent },
-    { path: '', component: AboutusComponent },
+    { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'aboutus', component: AboutusComponent },
+    { path: '**', component: NotfoundComponent }
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  notifications = [
+    { message: 'Hello, Netaji', time: '2m ago' },
+  ]
 
 }
